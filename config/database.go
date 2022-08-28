7cd3495e99b6100ddacd0f8551a5a51401809426
luxuetao <2982271907@qq.com>
2022-08-28 16:05:11 +0800

    updata dbconfig

---
+++ config/database.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type DbConfig struct {
+	Username   string
+	Password   string
+	Hostname   string
+	Port       string
+	Dbname     string
+	Charset    string
+	PareseTime string
+	Local      string
+}
+
+
+func GetDbConfig() DbConfig {
+	var dbConfig DbConfig
+	yamlFile, err := os.ReadFile("../conf/dbConfig.yaml")
+	if err != nil {
+		fmt.Println("read file error:" + err.Error())
+	}
+	err = yaml.Unmarshal(yamlFile, &dbConfig)
+	if err != nil {
+		fmt.Println("unmarshal error:" + err.Error())
+	}
+	return dbConfig
+}
