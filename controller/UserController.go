641a183951b2e4ce5fe5850de7a4d2a7e939f296
luxuetao <2982271907@qq.com>
2022-09-04 16:09:27 +0800

    updata some error

--- controller/UserController.go
+++ controller/UserController.go
@@ -1,17 +1,40 @@
 package controller
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/shawu21/test/common"
 	"github.com/shawu21/test/helper"
 	"github.com/shawu21/test/model"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetUserInfo(c *gin.Context) {
-	UserID := c.MustGet("user_id").(int)
-	user, err := model.GetUserInfo(UserID)
+	UserID := c.Query("id")
+	userID, err := strconv.Atoi(UserID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, helper.ApiReturn(common.CodeError, "id is not right", nil))
+		return
+	}
+	if userID == common.CheckSelf {
+		id := c.MustGet("user_id").(int)
+		user, err := model.GetUserInfo(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, helper.ApiReturn(common.CodeError, "查询用户信息失败", nil))
+			return
+		}
+		c.JSON(http.StatusOK, helper.ApiReturn(common.CodeSuccess, "查询用户信息成功", model.ViewUser{
+			Name:   user.Name,
+			QQ:     user.QQ,
+			Email:  user.Email,
+			Wechat: user.Wechat,
+			Tel:    user.Tel,
+		}))
+		return
+	}
+	user, err := model.GetUserInfo(userID)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, helper.ApiReturn(common.CodeError, "查询用户信息失败", nil))
 		return
